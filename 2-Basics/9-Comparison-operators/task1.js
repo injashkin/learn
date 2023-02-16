6eaae49eada4f23ae6315d820f85b4ab3c60aff6
injashkin <injashkin@gmail.com>
2023-02-16 10:07:41 +0300

    Solve 2.9 Comparison Operators

---
+++ 2-Basics/9-Comparison-operators/task1.js
@@ -0,0 +1,7 @@
+5 > 4 // true
+"ананас" > "яблоко" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // Мой ответ true, а должно быть false
+null === +"\n0\n" // false
